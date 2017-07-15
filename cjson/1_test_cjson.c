9c358cfc8775e801af7c79a4402d0f37c601befd
jinhongliang <jinhongliang@bupt.edu.cn>
2017-07-15 17:20:20 +0800

    add cjson frame

---
+++ cjson/1_test_cjson.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "cJSON.h"
+
+// read json data from 1_test_data.txt file
+int read_json_data_from_file(const char* filename, char* json_data) {
+}
+
+int main(void) {
+	return 0;
+}
---
+++ cjson/1_test_cjson.h
@@ -0,0 +1,6 @@
+#ifndef __1_TEST_CJSON_H__
+#define __1_TEST_CJSON_H__
+
+int read_json_data_from_file(const char*, char*);
+
+#endif
